6727aafd29a930ece94b734d45221cebc47c728a
Shadab <alamshadab9876543210@gmail.com>
2025-11-21 20:43:45 +0530

    added fixes

--- components/Navbar/page.tsx
+++ components/Navbar/page.tsx
@@ -144,7 +144,7 @@ const Navbar = () => {
 
   return (
     <header className="bg-white/80 backdrop-blur-xl border-b border-slate-200 sticky top-0 z-50 shadow-sm">
-      <div className="max-w-7xl mx-auto px-4 py-4">
+      <div className="max-w-5xl mx-auto px-4 py-4">
         <div className="flex items-center justify-center mb-4">
           <Link href={'/home'} className="text-4xl md:text-6xl font-bold bg-clip-text text-transparent flex">
             <span className="text-black">
