682a4990453a51f6908bfcc00ed99015aeff492b
Shadab <alamshadab9876543210@gmail.com>
2025-12-08 21:18:16 +0530

    added many fixes

--- convex/functions/newPosts/platforms/blogsiteFetchers/extractRss.ts
+++ convex/functions/newPosts/platforms/blogsiteFetchers/extractRss.ts
@@ -1,19 +1,17 @@
-"use node"
-
-import * as cheerio from "cheerio";
+"use node";
 
 export function extractRSSLinks(html: string, baseUrl: string): string[] {
-  const $ = cheerio.load(html);
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, "text/html");
+
   const feeds: string[] = [];
 
-  $('link[type="application/rss+xml"], link[type="application/atom+xml"]').each(
-    (_, el) => {
-      const href = $(el).attr("href");
-      if (href) feeds.push(href.startsWith("http") ? href : baseUrl + href);
-    }
-  );
+  doc.querySelectorAll('link[type="application/rss+xml"], link[type="application/atom+xml"]').forEach((el) => {
+    const href = el.getAttribute("href");
+    if (href) feeds.push(href.startsWith("http") ? href : new URL(href, baseUrl).href);
+  });
 
-  // Fallback guess patterns:
+  // fallback guesses
   const guesses = ["feed", "rss", "atom"].map((p) => `${baseUrl}/${p}`);
   return [...feeds, ...guesses];
 }
